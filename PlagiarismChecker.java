65ade3b8e820bb370bc52924cd8c61b77527bb2d
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-08-07 16:35:52 -0500

    Create PlagiarismChecker.java

---
+++ PlagiarismChecker.java
@@ -0,0 +1,11 @@
+public class PlagiarismChecker {
+  public boolean loadFiles(String[] paths) {
+    return false;
+  }
+  
+  public ResultChecker verifyPlagiarism(String path) {
+    ResultChecker result = null;
+    
+    return result;
+  }
+}
