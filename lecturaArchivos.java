98ef6cf64d7d85e0ae6323f4d4651d95d0fc4946
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2022-08-08 12:52:06 -0500

    Avance lectura de archivos

--- lecturaArchivos.java
+++ lecturaArchivos.java
@@ -1,5 +1,44 @@
-/*
-1. Cargar archivos (array)
-  1. Convertir todas las oraciones a conjuntos
-2. Eliminar palabras stop
-*/
+import java.io.*;
+import java.util.*;
+import javax.swing.*;
+
+public class lecturaArchivos {
+    JFileChooser fileChooser = new JFileChooser ();
+    ArrayList <File> fileList = new ArrayList <File> ();
+    String[] paths;
+    
+    
+    public void leerArchivos () {
+        fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+        
+        if (!fileChooser.isMultiSelectionEnabled()) {
+           fileChooser.setMultiSelectionEnabled(true);
+        }
+        
+        fileChooser.showOpenDialog(fileChooser);
+        
+        try {
+            for (File f : fileChooser.getSelectedFiles()) {
+                fileList.add(f);
+            }
+            
+            paths = new String[fileList.size()];
+            
+            for(int i = 0; i < fileList.size(); i++) {
+                paths[i] = fileList.get(i).getAbsolutePath();
+            }
+        } catch (NullPointerException e) {
+            System.out.println("No se ha seleccionado ningún fichero");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public ArrayList <File> GetFiles () {
+        return fileList;
+    }
+    
+    public String[] getPaths() {
+        return paths;
+    }
+}
