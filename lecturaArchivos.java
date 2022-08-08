95564626a01cf4dbd36d5d71551a6f471cd33312
mhuamanivar <93347594+mhuamanivar@users.noreply.github.com>
2022-08-08 13:16:15 -0500

    avance

--- lecturaArchivos.java
+++ lecturaArchivos.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 public class lecturaArchivos {
     JFileChooser fileChooser = new JFileChooser ();
     ArrayList <File> fileList = new ArrayList <File> ();
-    String[] archivos;
     String[] paths;
     
     
@@ -24,38 +23,19 @@ public class lecturaArchivos {
             }
             
             paths = new String[fileList.size()];
-            archivos = new String[fileList.size()];
             
             for(int i = 0; i < fileList.size(); i++) {
-                archivos[i] = "";
                 paths[i] = fileList.get(i).getAbsolutePath();
             }
             
-            int c = 0;
-            for(File f : fileList) {
-                Scanner sc = new Scanner(f);
-
-                while (sc.hasNextLine())
-                    archivos[c] += sc.nextLine() + "\n";
-                c++;
-            }
-            
         } catch (NullPointerException e) {
-            System.out.println("No se ha seleccionado ningún fichero");
+            System.out.println("No se ha seleccionado ningún archivo");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
     
-    public ArrayList <File> GetFiles () {
-        return fileList;
-    }
-    
     public String[] getPaths() {
         return paths;
-    }
-    
-    public String[] getArchivos() {
-        return archivos;
     }
 }
